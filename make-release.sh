149856021cfe3c7724ed8cca010d9086310df71d
VismutFO <p3lm2n1@mail.ru>
2023-02-14 20:00:19 +0300

    actions: first try

---
+++ make-release.sh
@@ -0,0 +1,13 @@
+mkdir deploy
+cp tea deploy/tea
+cp libtea.so deploy/libtea.so
+cp lib/binary.so deploy/binary.so
+cp lib/fs.so deploy/fs.so
+cp lib/gd.so deploy/gd.so
+cp lib/process.so deploy/process.so
+cp lib/pgsql.so deploy/pgsql.so
+cp lib/socket.so deploy/socket.so
+cp lib/tls.so deploy/tls.so
+cp lib/zlib.so deploy/zlib.so
+cp teajs.conf deploy/teajs.conf
+cp lib/snapshot_blob.bin deploy/snapshot_blob.bin
